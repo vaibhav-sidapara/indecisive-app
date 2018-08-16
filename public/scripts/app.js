e35467b128dca7edebc6018b3bebd0c6d7190bcf
Vaibhav Sidapara <vaibhav@tusitalabooks.com>
2018-08-16 19:44:59 +0800

    JSX Expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,59 @@
 console.log('App.js is running.');
 
 // JSX - JavaScript XML
+
+var app = {
+    'title': 'Indecisive App',
+    'subTitle': 'Random Generator'
+};
+
 var template = React.createElement(
-  'p',
-  null,
-  'This is JSX app.js.'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subTitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Vaibhav Sidapara'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 26'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Singapore'
+    )
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 console.log('App.js is running.');
 
 // JSX - JavaScript XML
-var template = <p>This is JSX app.js.</p>;
+
+var app = {
+    'title': 'Indecisive App',
+    'subTitle' : 'Random Generator',
+};
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subTitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var template2 = (
+    <div>
+        <h1>Vaibhav Sidapara</h1>
+        <p>Age: 26</p>
+        <p>Location: Singapore</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
