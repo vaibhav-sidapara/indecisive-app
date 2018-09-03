ac13cc5e2f2176290c4099d1ef510c2bb8d45b48
Vaibhav Sidapara <vaibhav@tusitalabooks.com>
2018-09-03 16:00:54 +0800

    ES6 Classes Part 2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -13,8 +13,50 @@ class Person {
     }
 }
 
-const me = new Person('Vaibhav Sidapara', 26);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
 
-const other = new Person();
-console.log(other.getDescription());
+    hasMajor() {
+        return !!this.major
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+
+        if(this.hasMajor()) {
+            description = `${description} Their major is ${this.major}`
+        }
+
+        return `${description}`;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreetings() {
+        let greetings = super.getGreetings();
+
+        if(this.hasHomeLocation()) {
+            greetings = `${greetings} I am visiting from ${this.homeLocation}`
+        }
+
+        return greetings;
+    }
+}
+
+const me = new Traveler('Vaibhav Sidapara', 26, 'Singapore');
+console.log(me.getGreetings());
+
+const other = new Traveler();
+console.log(other.getGreetings());
